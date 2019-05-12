57b7b6ce6e8e162dc0c1789b43e4a68efa699016
Haley DeKeyser <hcd09@live.com>
2019-05-12 03:17:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,7 @@ def my_collect(array)
     
     while i < array.length
       yield array[i]
-      array { |x| new_array << yield(x) }
+      array[i] { |x| new_array << yield(x) }
       i += 1
     end
     
