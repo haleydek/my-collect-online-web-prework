c94484dce35e61d11f38c15773d3742575477acd
Haley DeKeyser <hcd09@live.com>
2019-05-12 02:51:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@ def my_collect(array)
     i = 0
     new_array = []
     
-    element = while i < array.length
-      yield array[i]
+    while i < array.length
+      element = yield array[i]
       i += 1
     end
     
