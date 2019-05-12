a12d4ae104b410d509d4e05daca83bc9e1b85275
Haley DeKeyser <hcd09@live.com>
2019-05-12 02:22:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,14 @@
 def my_collect(array)
+  if block_given?
+    i = 0
+    
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    
+    
   
+  |languages| languages.capitalize
 end
 
